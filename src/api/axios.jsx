096cc915f871f37d0e1407b3d1cfc9824e845c00
Divine <rickybanky4@gmail.com>
2026-06-01 19:38:04 +0100

    axios review and update

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -3,6 +3,9 @@ import axios from 'axios';
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
-});
+  
+}
+);
+console.log("API URL:", import.meta.env.VITE_API_URL);
 
 export default API;
