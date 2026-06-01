11c1582c4c273cd2dfeb76984b56814bf14cf36b
Divine <rickybanky4@gmail.com>
2026-06-01 19:43:51 +0100

    axios re-update

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'https://golden-castle-school-api.onrender.com/api',
   withCredentials: true,
   
 }
 );
-console.log("API URL:", import.meta.env.VITE_API_URL);
 
 export default API;
